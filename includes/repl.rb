c5ef1cfe70657f2a3328b7e2028194587ca0fc78
Chris Eich <cjeich@gmail.com>
2016-03-28 05:04:28 -0700

    Cleaning up repl loop
    Repl loop now conforms with spec when input piped from STDIN

--- includes/repl.rb
+++ includes/repl.rb
@@ -1,19 +1,17 @@
 # REPL is responsible for:
 #  - starting the read, evaluate, print loop
 #  - handling signals passed in by the user and input exceptions
+require 'readline'
 
 class Repl
   def self.repl(prompt: '> ')
-    print prompt
-    while input = gets
-      break unless input && input != 'f\n'
-      input = input.chomp
+    loop do
+      input = Readline.readline prompt
 
+      break unless input && input != 'f'
       puts process_input(input, &Proc.new)
-      print prompt
     end
 
-    # Catch User input like ^C and exit without throwing errors
     rescue SystemExit, Interrupt
       exit
   end
